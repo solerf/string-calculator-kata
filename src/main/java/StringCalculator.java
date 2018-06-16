4fbc31ef725f961efc5464b7bcb0298e0212eaf8
Felipe Soler <soler.felipe@gmail.com>
2018-06-16 18:16:57 +0200

    adding step 1 and step 2

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * @author Felipe Soler
+ * @date 03/06/2018
+ */
+public class StringCalculator {
+
+    public int add(String numbers) {
+        if(numbers == null || numbers.isEmpty()){
+            return 0;
+        }
+        return Arrays.stream(numbers.split(","))
+                .collect(Collectors.summingInt(Integer::valueOf));
+    }
+
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,55 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Felipe Soler
+ * @date 03/06/2018
+ */
+public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void before(){
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void test_step1_empty_string(){
+        int actual = stringCalculator.add("");
+        assertThat(actual, is(0));
+    }
+
+    @Test
+    public void test_step1_one_number(){
+        int actual = stringCalculator.add("1");
+        assertThat(actual, is(1));
+    }
+
+    @Test
+    public void test_step1_two_numbers_1(){
+        int actual = stringCalculator.add("1,2");
+        assertThat(actual, is(3));
+    }
+
+    @Test
+    public void test_step1_two_numbers_2(){
+        int actual = stringCalculator.add("3,2");
+        assertThat(actual, is(5));
+    }
+
+    @Test
+    public void test_step1_null_string(){
+        int actual = stringCalculator.add(null);
+        assertThat(actual, is(0));
+    }
+
+    @Test
+    public void test_step2_multiple_numbers(){
+        int actual = stringCalculator.add("3,2,1,1,5");
+        assertThat(actual, is(12));
+    }
+}
